bc48695a6fc4773f291e9a22eeb816902600885e
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2023-03-10 14:57:40 +0300

    ....

--- lib/main.dart
+++ lib/main.dart
@@ -5,4 +5,6 @@ import 'my_app.dart';
 void main() {
   runApp(const MyApp());
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 }
